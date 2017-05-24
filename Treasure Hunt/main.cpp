8ac2279a39e24d7ad10a2ecc29548ba95ce9b950
Robyn Murray <rnmurray@ucdavis.edu>
2017-05-24 03:33:24 -0700

    added files
    added files

---
+++ Treasure Hunt/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Treasure Hunt
+//
+//  Created by Robyn Murray on 7/30/15.
+//  Copyright (c) 2015 Robyn Murray. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Would you like to play a game? (y/n)\n";
+    
+    
+}
